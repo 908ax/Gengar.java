5173bb9bc41c1f2c114e03468452441040a0c760
908ax <leonbalteriano@gmail.com>
2024-11-19 09:01:55 +0100

    Commit inicial

---
+++ Gengar.java
@@ -0,0 +1,34 @@
+public class Gengar {
+    private int peso;
+    private String apodo;
+    private boolean variacolor;
+    public Gengar(String elApodo, int elPeso) {
+        this.apodo = elApodo;
+        this.peso = elPeso;
+        this.variacolor = false;
+    }
+    
+    public String getApodo() {
+        return apodo;
+    }
+    
+    public int getPeso() {
+        return peso;
+    }
+
+    public boolean isVariacolor() {
+        return variacolor;
+    }
+
+    public void setApodo(String nuevoApodo) {
+        this.apodo = nuevoApodo;
+    }
+
+    public void engordar(int comida) {
+        this.peso += comida;
+    }
+
+    public void alternarVariacolor() {
+        this.variacolor = !this.variacolor;
+    }
+}
