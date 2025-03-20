ca72b042b829e328b31fcfe8fe645a161aae20c3
Leon <leonbalteriano@gmail.com>
2025-03-20 18:58:12 +0100

    tamaño

--- Gengar.java
+++ Gengar.java
@@ -2,10 +2,16 @@ public class Gengar {
     private int peso;
     private String apodo;
     private boolean variacolor;
+    private boolean gigante;
     public Gengar(String elApodo, int elPeso) {
         this.apodo = elApodo;
         this.peso = elPeso;
         this.variacolor = false;
+        this.gigante = true;
+    }
+    
+    public boolean isGigante() {
+        return gigante;
     }
     
     public String getApodo() {
@@ -32,17 +38,29 @@ public class Gengar {
         this.variacolor = !this.variacolor;
     }
     
+    public void alternarGigante() {
+        this.gigante = !this.gigante;
+    }
+    
     public void imprimirGengar(){
         System.out.println(getEstadoGengar());
     }
     
     public String getEstadoGengar() {
         String estadoVariacolor;
+        String tamaño;
         if (variacolor) {
             estadoVariacolor = "Verdadero";
             } else {
             estadoVariacolor = "Falso";
         }
-        return "Apodo: " + apodo + " | Peso: " + peso + " | Variacolor: " + estadoVariacolor;
+        
+        if (gigante) {
+            tamaño = "Verdadero";
+            } else {
+            tamaño = "Falso";
+        }
+        
+        return "Apodo: " + apodo + " | Peso: " + peso + " | Variacolor: " + estadoVariacolor + " | Es gigante: " + tamaño;
     }
 }
